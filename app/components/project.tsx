97bc47f824cd14a9786310be9edd72c942803b32
nafamubarokhusni <nafamubarokhusni@gmail.com>
2026-01-31 22:54:10 +0700

    feat: add ArrowIcon component and update Project component to include it

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -1,3 +1,4 @@
+import { ArrowIcon } from 'app/components/icons/arrow'
 import Link from 'next/link'
 
 const projects = [
@@ -26,10 +27,13 @@ export function Project() {
           target="_blank"
           rel="noreferrer"
         >
-          <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-            <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-              {project.name}
-            </p>
+          <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 items-center">
+            <div className="flex items-center gap-2">
+              <p className="text-neutral-900 dark:text-neutral-300 tracking-tight dark:hover:text-neutral-100">
+                {project.name}
+              </p>
+              <ArrowIcon />
+            </div>
             {/* <p className="text-neutral-600 dark:text-neutral-400">
               {project.url}
             </p> */}
