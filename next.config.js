8565223f3de7ba97c941e9ce29d7d19942e9c086
nafamubarokhusni <nafamubarokhusni@gmail.com>
2026-01-31 19:01:56 +0700

    feat: remove basePath and assetPrefix for cleaner static export configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // enables static exports
-  basePath: '/nafamubarokhusni.github.io', // replace with your repository name
-  assetPrefix: '/nafamubarokhusni.github.io/', // replace with your repository name
 };
 
 module.exports = nextConfig;
